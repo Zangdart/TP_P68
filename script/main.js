b34ba868e7e8e8cd8f557cc79927002700f6f141
dcastane <davy.castanet@gmail.com>
2016-08-26 12:28:55 -0400

    Version finale dt+mb

--- script/main.js
+++ script/main.js
@@ -75,4 +75,26 @@ $(function() {
     //     declencher_auto(false);
     // }
 
-});    
+});
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// document.querySelector('.icon').addEventListener('click', function(){
+//     var x = document.querySelector("#menu_principal").classList.toggle('open');
+// });
